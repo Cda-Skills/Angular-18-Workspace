94813890b162bbe1c266172f97d37260200e3a58
CDA-SKILLS <cda.skills@gmail.com>
2024-10-10 22:17:29 +0200

    Maj du dernier commit

--- projects/learning-angular18/src/app/templateForm/formulaire/formulaire.component.ts
+++ projects/learning-angular18/src/app/templateForm/formulaire/formulaire.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { User } from '../../models/user.model';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule} from '@angular/common';
@@ -11,29 +11,15 @@ import { CommonModule} from '@angular/common';
   templateUrl: './formulaire.component.html',
   styleUrl: './formulaire.component.css'
 })
-export class FormulaireComponent implements OnInit{
+export class FormulaireComponent {
 
   user: User = new User();
 
-
-  ngOnInit(): void {}
-
-  constructor(){
-
-  }
-
   saveFromData(registerForm: NgForm){
     console.log('Le bouton fonctionne');
-    console.log(this.user);
+    // console.log(this.user);
     // console.log(registerForm.form);
     console.log(registerForm.value);
-    if (registerForm.invalid) {
-      return;
+   
   }
-
-  // alert('SUCCESS!! :-)\n\n' + JSON.stringify(registerForm.value, null, 4));
-  }
-
-
-  
 }
